644f33df8f207682a405e95ca136713554a62a5f
Marco Gnecchi <marco.gnecchi@if65.it>
2022-02-22 09:23:10 +0100

    inserita vendita fittizia per promozione 51

---
+++ test/controllo.php
@@ -0,0 +1,85 @@
+<?php
+
+$filePath = realpath(__DIR__) . "/dc/";
+//$filePath = "/Users/if65/Desktop/Invio Cosimo/invio_05/";
+
+$fileList = scandir($filePath);
+
+foreach ($fileList as $file) {
+	if (preg_match("/^DC.*\.DAT$/", $file)) {
+		$transazioneAperta = false;
+		$text = file_get_contents($filePath . $file);
+		$rows = explode("\n", $text);
+		if ($rows) {
+			foreach ($rows as $i => $row) {
+
+				/** inizio transazione */
+				if (preg_match("/^.{21}004(\d{4})(\d{4})/", $row, $m)) {
+					$transazione = $m[1];
+					$cassa = $m[2];
+					$totaleCalcolato = 0;
+					$transazioneAperta = true;
+
+					if ($transazione == "7667") {
+						echo "\n";
+					}
+				}
+
+				if ($transazioneAperta) {
+					if (preg_match("/^.{21}1001.{18}(\d{7})(\d{2})/", $row, $m)) {
+						$totaleCalcolato += $m[1] . '.' . $m[2];
+					}
+					if (preg_match("/^.{21}1013.{18}(\d{7})(\d{2})/", $row, $m)) {
+						$totaleCalcolato -= $m[1] . '.' . $m[2];
+					}
+					if (preg_match("/^.{21}1088.{18}(\d{7})(\d{2})/", $row, $m)) {
+						$totaleCalcolato -= $m[1] . '.' . $m[2];
+					}
+					if (preg_match("/^.{21}1091.{18}(\d{7})(\d{2})/", $row, $m)) {
+						$totaleCalcolato -= $m[1] . '.' . $m[2];
+					}
+					if (preg_match("/^.{21}1085.{18}(\d{7})(\d{2})/", $row, $m)) {
+						$totaleCalcolato -= $m[1] . '.' . $m[2];
+					}
+					if (preg_match("/^.{21}1086.{18}(\d{7})(\d{2})/", $row, $m)) {
+						$totaleCalcolato -= $m[1] . '.' . $m[2];
+					}
+					if (preg_match("/^.{21}1087.{18}(\d{7})(\d{2})/", $row, $m)) {
+						$totaleCalcolato -= $m[1] . '.' . $m[2];
+					}
+					if (preg_match("/^.{21}1094.{18}(\d{7})(\d{2})/", $row, $m)) {
+						$totaleCalcolato -= $m[1] . '.' . $m[2];
+					}
+					if (preg_match("/^.{21}1055.{18}(\d{7})(\d{2})/", $row, $m)) {
+						$totaleCalcolato -= $m[1] . '.' . $m[2];
+					}
+					if (preg_match("/^.{21}1051.{18}(\d{7})(\d{2})/", $row, $m)) {
+						$totaleCalcolato -= $m[1] . '.' . $m[2];
+					}
+					if (preg_match("/^.{21}1050.{18}(\d{7})(\d{2})/", $row, $m)) {
+						$totaleCalcolato -= $m[1] . '.' . $m[2];
+					}
+					if (preg_match("/^.{21}1062.{18}(\d{7})(\d{2})/", $row, $m)) {
+						$totaleCalcolato -= $m[1] . '.' . $m[2];
+					}
+
+					if (preg_match("/^.{21}1000.{18}(\d{7})(\d{2})/", $row, $m)) {
+						$totaleCalcolato -= $m[1] . '.' . $m[2];
+					}
+				}
+
+				if (preg_match("/^.{21}1020.{18}(\d{7})(\d{2})/", $row, $m)) {
+					$totale = ($m[1] . '.' . $m[2]) * 1;
+					$transazioneAperta = false;
+
+					if (round($totaleCalcolato - $totale,2)) {
+						echo "$file, $transazione/$cassa : $totaleCalcolato <=> $totale\n";
+					}
+				}
+
+			}
+		}
+	}
+}
+
+echo "\n";
